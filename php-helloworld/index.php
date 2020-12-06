d46987e211c05fd79bfa3310b8741f599a95ae72
Student User <student@workstation.lab.example.com>
2020-12-06 01:42:04 -0500

    vinayan updated

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "vinayan N Thilakan" . "\n";
-echo date("l") . "<br>";
-
-// Prints the day, date, month, year, time, AM or PM
-echo date("l jS \of F Y h:i:s A");
+print "A change is a coming!\n";
 ?>
